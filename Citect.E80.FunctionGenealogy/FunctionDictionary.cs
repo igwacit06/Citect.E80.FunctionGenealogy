aa4dc621a9726a5608e73888a24c8990b4659d52
Ignatius Wong <ignatiusw76@gmail.com>
2019-05-09 16:25:13 +0800

    initial build

---
+++ Citect.E80.FunctionGenealogy/FunctionDictionary.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Citect.E80.FunctionGenealogy
+{
+    public class FunctionDictionary
+    {
+        /// <summary>
+        /// get list of functions from ci file in root directory (includes subs)
+        /// </summary>
+        /// <param name="root"></param>
+        public static List<CicodeFunctions> GetFunctions(string root)
+        {
+            //get list of files
+            var searchList = Directory.GetFiles(root, "st_*.ci", SearchOption.AllDirectories);
+            var cicodefunctions = new List<CicodeFunctions>();
+            
+            foreach (var file in searchList)
+            {
+                //open the ci file and get list of functions
+                var lineNum = 1;
+                var funcDefInNextLine = false;
+                var funcLoc = new Dictionary<int, string>();
+                var functionList = new CicodeFunctions() { FileName = file };
+
+                using (var streamReader = new StreamReader(file))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        try
+                        {
+                            string funcName = string.Empty;
+                            var line = streamReader.ReadLine();
+                            if (string.IsNullOrEmpty(line) || line.StartsWith("//")) continue;
+
+                            if (funcDefInNextLine)
+                            {
+                                if (line.IndexOf('(') > 0)
+                                {
+                                    funcName = line.Substring(0, line.IndexOf('('));
+                                    if (funcName.Length > 1)
+                                        if (!funcLoc.ContainsKey(lineNum))
+                                            funcLoc.Add(lineNum, funcName.Trim());
+                                }
+                                funcDefInNextLine = false;
+                            }
+
+                            //use regex to find function definition?
+                            if (line.Contains("FUNCTION"))
+                            {
+                                //name of function follows the keyword
+                                if (line.IndexOf('(') > 0)
+                                {
+                                    var first = line.IndexOf("FUNCTION") + "FUNCTION".Length;
+                                    funcName = line.Substring(first, line.IndexOf('(') - first);
+                                    if (funcName.Length > 1)
+                                        if (!funcLoc.ContainsKey(lineNum))
+                                            funcLoc.Add(lineNum, funcName.Trim());
+                                }
+                                else
+                                    funcDefInNextLine = true;
+                            }
+
+                            if (funcName.Length > 1)
+                            {
+                                if (!funcLoc.ContainsKey(lineNum))
+                                    funcLoc.Add(lineNum, funcName.Trim());
+                            }
+                            lineNum++;
+                        }
+                        catch (Exception ex)
+                        {
+                            
+                        }
+                    }
+                }
+                functionList.FunctionLocations = funcLoc;
+                cicodefunctions.Add(functionList);
+            }
+            return cicodefunctions;
+        }
+
+        public static bool CheckFunctionUsedInDBF(string root)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+   
+    public class CicodeFunctions
+    {
+        public string FileName { get; set; }
+        public Dictionary<int, string> FunctionLocations { get; set; }
+    }        
+}
---
+++ FunctionGenealogyTest/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Citect.E80.FunctionGenealogyTest
+{
+    [TestClass]
+    public class FunctionDictionaryTest
+    {
+        public TestContext TestContext { get; set; }
+
+
+        [TestMethod]
+        public void FunctionDictionaryTest_ShouldNotBeNull()
+        {
+
+            var path = Path.GetDirectoryName(@"C:\ProgramData\Schneider Electric\Citect SCADA 2018\User");
+
+            var functions = FunctionGenealogy.FunctionDictionary.GetFunctions(path);
+
+            Assert.IsNotNull(functions, "cannot be null");
+
+            foreach (var data in functions)
+            {
+                Assert.IsNotNull(data.FunctionLocations,"function locations cannot be null");
+                foreach (var kvp in data.FunctionLocations)
+                {
+                    TestContext.WriteLine("file:{0},line:{1},funcName:{2}", data.FileName, kvp.Key, kvp.Value);
+                }
+            }
+        }
+    }
+}
