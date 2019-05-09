cb7a3392d6fb357ea79c67fc766e441e9f5edb69
Ignatius Wong <ignatiusw76@gmail.com>
2019-05-09 14:09:01 +0800

    Add project files.

---
+++ Citect.E80.FunctionGenealogy/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Citect.E80.FunctionGenealogy
+{
+    public class Class1
+    {
+    }
+}
