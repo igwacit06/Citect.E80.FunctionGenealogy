9fe6b4fdff669bbc520bd651583e499ea19a0c4e
Ignatius Wong <ignatiusw76@gmail.com>
2019-09-02 14:11:55 +0800

    test

---
+++ Citect.E80.OPCConnectivity/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Citect.E80.OPCConnectivity
+{
+    public class Class1
+    {
+    }
+}
